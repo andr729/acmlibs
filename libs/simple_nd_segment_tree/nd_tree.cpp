a5660a8a8b900104f4b433f235ac6ce6ee78332a
Andrzej <andrzej@rpi.pl>
2022-08-01 13:23:53 +0200

    NdTrees: nd shapes

--- libs/simple_nd_segment_tree/nd_tree.cpp
+++ libs/simple_nd_segment_tree/nd_tree.cpp
@@ -1,8 +1,71 @@
+/**
+ * @file nd_tree.cpp
+ * @brief TBD
+ */
+
 #include <iostream>
 
+using dim_t = uint64_t;
+
+// simple range types:
+
+template<uint n>
+struct RangeND {
+    dim_t from, to; // inclusive
+    RangeND<n - 1> next;
+};
+
+template<>
+struct RangeND<0> {
+    dim_t from, to;
+};
+
+template<uint n>
+struct ShapeND {
+    dim_t size;
+    ShapeND<n - 1> next;
+};
+
+template<>
+struct ShapeND<0> {
+    dim_t size;
+};
+
+template<uint n, class ValType>
+struct TreeNode {
+
+};
+
+template<class ValType>
+struct TreeNode<0, ValType> {
+    
+};
+
+template<uint n, class ValType>
+struct TreeNDAux {
+    const ShapeND<n>& shape;
+    TreeND(const  ShapeND<n>& shape): shape(shape) {
+
+    }
+};
+
+// template<class ValType>
+// struct TreeNDAux<0, ValType> {
+
+// };
+
+template<uint n, class ValType>
+class TreeND {
+    ShapeND<n> shape;
+    TreeNDAux<n> tree;
+    TreeND(ShapeND<n> shape):
+        shape(shape),
+        tree(shape) {
+    }
+};
 
 
 
 int main() {
-    
+    RangeND<3> hmm;
 }
